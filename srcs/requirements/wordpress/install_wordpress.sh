e9df05083b30a2b569103b92edc64c03e8eafb61
Paulo <pauldos-@Inception.42porto.com>
2025-11-12 19:03:43 +0000

    Backup sync 2025-11-12

--- srcs/requirements/wordpress/install_wordpress.sh
+++ srcs/requirements/wordpress/install_wordpress.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+
 cd /var/www/html
+
+# Download WP-CLI if missing
 if [ ! -f wp-cli.phar ]; then
 	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 	chmod +x wp-cli.phar
@@ -12,13 +15,24 @@ fi
 if [ ! -f wp-config.php ]; then
 	./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
 fi
-
-if ! ./wp-cli.phar core is-installed --allow-root; then
+if ! ./wp-cli.phar core is-installed --allow-root &>/dev/null; then
 	./wp-cli.phar core install --url=localhost --title=inception --admin_user=pauldos- \
-	--admin_password=123456 --admin_email=pauldos-@inception.com --allow-root
+		--admin_password=123456 --admin_email=pauldos-@inception.com --allow-root
+fi
 
-	./wp-cli.phar core install --url=pauldos-.42.fr --title=inception --admin_user=pauldos- \
-	--admin_password=123456 --admin_email=pauldos-@inception --allow-root
+CUSTOM_USER="pauldos-"
+CUSTOM_PASS="123456"
+CUSTOM_EMAIL="pauldos-@inception.com"
+
+# Check if user exists
+if ! ./wp-cli.phar user get "$CUSTOM_USER" --allow-root &>/dev/null; then
+    # Create user if missing
+    ./wp-cli.phar user create "$CUSTOM_USER" "$CUSTOM_EMAIL" \
+        --role=administrator --user_pass="$CUSTOM_PASS" --allow-root
+else
+    # Update password and email if user exists
+    ./wp-cli.phar user update "$CUSTOM_USER" \
+        --user_pass="$CUSTOM_PASS" --user_email="$CUSTOM_EMAIL" --allow-root
 fi
 
 php-fpm8.2 -F
