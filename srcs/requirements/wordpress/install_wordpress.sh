f673b8088a04b62d8a503f3ff64f9a754528c795
Paulo <pauldos-@Inception.42porto.com>
2025-11-12 16:40:46 +0000

    Backup sync 2025-11-12

--- srcs/requirements/wordpress/install_wordpress.sh
+++ srcs/requirements/wordpress/install_wordpress.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 cd /var/www/html
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+if [ ! -f wp-cli.phar ]; then
+	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+	chmod +x wp-cli.phar
+fi
+
+if [ ! -f index.php ]; then
+	./wp-cli.phar core download --allow-root
+fi
+
+if [ ! -f wp-config.php ]; then
+	./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
+fi
+
+if ! ./wp-cli.phar core is-installed --allow-root; then
+	./wp-cli.phar core install --url=localhost --title=inception --admin_user=pauldos- \
+	--admin_password=123456 --admin_email=pauldos-@inception.com --allow-root
+
+	./wp-cli.phar core install --url=pauldos-.42.fr --title=inception --admin_user=pauldos- \
+	--admin_password=123456 --admin_email=pauldos-@inception --allow-root
+fi
 
 php-fpm8.2 -F
